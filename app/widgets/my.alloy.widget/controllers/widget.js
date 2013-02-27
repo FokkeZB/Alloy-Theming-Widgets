a5cdc33ab028f3cc2e45940b0b5c3528cc4227cc
Fokke Zandbergen <mail@fokkezb.nl>
2013-02-27 13:45:58 +0100

    Shorter JS version

--- app/widgets/my.alloy.widget/controllers/widget.js
+++ app/widgets/my.alloy.widget/controllers/widget.js
@@ -1,15 +1,13 @@
 var args = arguments[0] || {};
 
 if (args.styles) {
-	var properties = {};
 	_.each(args.styles, function(value,property) {
 		if (typeof value === 'object') {
 			$[property].applyProperties(value);
-		} else {
-			properties[property] = value;
+			delete args.styles[property];
 		}
 	});
-	if (_.size(properties) > 0) {
-		$.getView().applyProperties(properties);
+	if (_.size(args.styles) > 0) {
+		$.getView().applyProperties(args.styles);
 	}
 }
